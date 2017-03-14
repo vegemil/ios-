08ee8ddd6620461083d6f22f20f32f178fa1a1ae
으나 :) <euna501@gmail.com>
2017-03-14 22:01:01 +0900

    2장 PickerView
    선택하면 이미지 뷰에 이미지 출력하도록 수정

---
+++ PickerView/PickerView/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by 정은아 on 2017. 3. 14..
+//  Copyright © 2017년 정은아. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate {
+    @IBOutlet weak var pickerImage: UIPickerView!
+    @IBOutlet weak var lblImageFileName: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let MAX_ARRAY_NUM = 5
+    let PICKER_VIEW_COLUMN = 1
+    var imageFileName = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"]
+    var imageArray = [UIImage?]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for i in 0 ..< MAX_ARRAY_NUM{
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lblImageFileName.text = imageFileName[0]
+        imageView.image = imageArray[0]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfComponentsInPickerView(pickerView : UIPickerView!) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return imageFileName[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lblImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
+    }
+}
+
